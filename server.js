9c7cb4342fb0017c9fe5b2ef418adb764a1dc6e6
Oleh Tymoshenko <olegtymoshenko2000@gmail.com>
2020-11-05 20:14:40 +0200

    Fix 2 server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-with-fire-base'));
+app.use(express.static(__dirname + '/dist/angularWithFireBase'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-with-fire-base/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angularWithFireBase/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
